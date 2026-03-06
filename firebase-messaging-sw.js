dae7cfd2edd0d31e320d759f19531d331868c3f3
yoojk72 <yoojk72@gmail.com>
2026-03-06 07:17:00 +0000

    FCM 설정 완료

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/12.10.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/12.10.0/firebase-messaging-compat.js");
+importScripts('https://www.gstatic.com/firebasejs/12.10.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/12.10.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyCgMXr-TqIn3jzWNLyBMirtqqdS0JQZN4M",
